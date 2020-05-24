4dbb46b0a2eee6f029f06f1f92216a97fbc5a289
Helena Johansson <helena@bulle.nu>
2020-05-24 21:21:17 +0200

    improved styling

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -40,7 +40,7 @@ const ContactItem = ({ contact }) => {
         )}
       </ul>
       <p>
-        <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
+        <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>{' '}{' '}
         <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
       </p>
     </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = ({ title, icon }) => {
 
   const authLinks = (
     <Fragment>
-      <li>Hello, {user && user.name}</li>
+      <li>Hello, {user && user.name}</li>{' '}{' '}
       <li>
         <a onClick={onLogout} href="#!">
           <i className="fas fa-sign-out-alt"></i>
